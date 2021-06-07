c125c984edb6041f6880c6ca16552ceac562802c
Artykbay <a.zhumakulov@kit.systems>
2021-06-07 17:20:11 +0600

    initial version

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/zh-artykbay/usinglib
+
+go 1.16
+
+require github.com/zh-artykbay/hw2lib v1.0.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/zh-artykbay/hw2lib"
+)
+
+func main() {
+	fmt.Println(hw2lib.Tolower("SGFDSG HDHGH"))
+	fmt.Println(hw2lib.Toupper("qwrtyrut"))
+}
